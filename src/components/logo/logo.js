323c2cefd389de1f2e28466a5a4d2e0c47994caa
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-05-26 11:29:32 -0300

    Criação de menu responsivo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -3,7 +3,7 @@ function typeWrite(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 150 * i)
+        setTimeout(() => elemento.innerHTML += letra, 300 * i)
     });
 }
 
