a6ab2fa017e8ec6fd0f1f84fd272926b69ea3941
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-05-31 13:44:55 -0300

    Adicionado food, interações dentro da página e links externos

---
+++ src/components/menu/scroll.js
@@ -0,0 +1,18 @@
+$('.infoButtons a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+$('.menu a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
